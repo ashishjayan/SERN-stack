32fddceea657e88d3b583b97cf9b80cce2e9dd11
Ananda Poudel <apoudel@stcloudstate.edu>
2018-11-04 17:56:29 -0600

    Deploy to aws

---
+++ client/build/precache-manifest.e1dfb82036e4a1346efbb2d9f28dd1c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8029220e578aa0875f31",
+    "url": "/static/js/main.8029220e.chunk.js"
+  },
+  {
+    "revision": "fe8efb1142d588df1c77",
+    "url": "/static/js/1.fe8efb11.chunk.js"
+  },
+  {
+    "revision": "8029220e578aa0875f31",
+    "url": "/static/css/main.4d941cf8.chunk.css"
+  },
+  {
+    "revision": "fe8efb1142d588df1c77",
+    "url": "/static/css/1.a665d90d.chunk.css"
+  },
+  {
+    "revision": "44f53c814a1a56ca70ead57ad2f63d57",
+    "url": "/index.html"
+  }
+];
